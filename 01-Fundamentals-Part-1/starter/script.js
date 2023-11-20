8b412b029fafc63364f7f8df9826151c3c3d2cce
brunoforlin <129110112+brunoforlin@users.noreply.github.com>
2023-11-20 20:12:37 -0300

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,45 @@ let js = "amazing"
 if (js === "amazing") alert('javascript is amazing')
 console.log(50-20-4);
   
+let javascriptIsFun = true;
+console.log (javascriptIsFun)
+console.log (typeof true)
+console.log (typeof 23)
+console.log (typeof 'Jonas')
+
+javascriptIsFun = 'yes!'
+
+//para comentar, só colocar as duas barras, js é tipagem dinamica, ou seja, ela identifica automatico que
+//identifica sozinha os tipos primitivos das variáveis, se é boolean, string, ou numero.
+
+// variaveis const, let e var. let serve pra variaveir que mudam ,const imutaveis, var é legado
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990; nao pode no js.
+*/
+
+const now = 2037;
+const ageBruno = now - 2008;
+const ageSara = now - 2016;
+console.log(ageBruno, ageSara)
+console.log(ageBruno / 2, ageBruno * 2, 2**3)
+// ** significa exponencial
+
+const firstName = 'bruno'
+const lastName = 'forlin'
+console.log(firstName + ' ' + lastName)
+//assigment operators
+let x = 10 +5;
+x += 10;
+x*= 4;
+x++;
+x--;
+console.log(x);
+
+//comparisson operators
+console.log (ageSara >= 18)
+
