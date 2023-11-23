eac4a9026cc0f588512e833ec0db56d93efb2d13
brunoforlin <129110112+brunoforlin@users.noreply.github.com>
2023-11-23 19:28:43 -0300

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing"
 if (js === "amazing") alert('javascript is amazing')
 console.log(50-20-4);
@@ -8,7 +9,7 @@ console.log (typeof true)
 console.log (typeof 23)
 console.log (typeof 'Jonas')
 
-javascriptIsFun = 'yes!'
+javascriptIsFun = 'yes!' */
 
 //para comentar, só colocar as duas barras, js é tipagem dinamica, ou seja, ela identifica automatico que
 //identifica sozinha os tipos primitivos das variáveis, se é boolean, string, ou numero.
@@ -80,7 +81,7 @@ if (isOldEnough) {
     console.log(`bruno can start driving license`)
 }
 */
-
+/*
 const age = 19;
 
 if (age >= 18) {
@@ -99,4 +100,90 @@ if (birthYear <= 2000) {
 } else {
     century = 21;
 }
-console.log (century)
+console.log (century) */
+
+/*Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".*/
+
+
+/*
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`"John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
+
+//mudar string pra numero e vice e versa
+
+const inputYear = '1991';
+
+console.log(Number(inputYear + 18)); // POR EXEMPLO, PODE-SE SOMAR NUMEROS COLOCANDO O Number, ou o contrario usando String
+console.log (String(23));
+
+*/
+/*
+const favNumber = Number(prompt("what's your favorite number ?"));
+console.log( typeof favNumber)
+
+
+
+//and && or || - not !
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+const isTired = false;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log("Bruno is able to drive.")
+} else {
+    console.log("Someone else should drive...")
+} */
+/*
+1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+"Dolphins win the trophy" if Dolphins win, or
+
+"Koalas win the trophy" if Koalas win, or
+
+"Both win the trophy" if their average scores are equal.
+
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+
+3. tem que ter no mimino 100 pontos
+
+*/
+
+const scoreDolphins = (96 + 108 + 45) / 3;
+const scoreKoalas = (96 + 108 + 39) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log("Dolphins win the trophy"); }
+    else if(scoreKoalas > scoreDolphins && scoreKoalas >=100) { console.log("Koalas win the trophy");
+} else if(scoreDolphins===scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log("Both win the trophy");
+} else {
+    console.log("No team win the trophy");
+}
