f88147b08e80f9049a217bd27175b2a79ea0556a
brunoforlin <129110112+brunoforlin@users.noreply.github.com>
2023-11-29 10:41:12 -0300

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -18,7 +18,7 @@ function logger() {
  console.log(resultado,resultado2)
  */
 
- function calcAge1(birthyear) {
+ /*function calcAge1(birthyear) {
     return 2037 - birthyear;
  }
 
@@ -37,3 +37,49 @@ function logger() {
  const calcAge3 = birthday => 2037 - birthday;
  const age3 = calcAge3(1986);
  console.log(age3);
+
+ const yearUntilRetirement = birthday => {
+   const age = 2023 - birthday;
+   const retirement = 65 - age;
+   return retirement;
+   
+ }
+
+ console.log(yearUntilRetirement(1986)) */
+
+ //fruit processor
+
+ const cutPieces = function (fruit){
+   return fruit * 4;
+ }
+
+ function returnJuice(apples, oranges) {
+   const applePieces = cutPieces(apples);
+   const orangesPieces = cutPieces(oranges);
+   const juice = `Juice with ${applePieces} apples and ${orangesPieces} oranges.`;
+   return juice;
+ }
+
+ const sucos = returnJuice(5,6);
+ console.log(sucos);
+
+
+ // 
+const calcAge = function (year){
+   return 2023 - year;
+}
+
+ const yearsUntilRetirement = function(birthYear, firstName) {
+   const age = calcAge(birthYear);
+   const retirement = 65 - age;
+
+   if (retirement > 0 ) {
+      console.log(`${firstName} retires in ${retirement} years.`);
+      return retirement;
+   } else {
+      console.log(`${firstName} has already retired.`);
+   }
+ }
+
+ console.log(yearsUntilRetirement(1986, 'Bruno'));
+ console.log(yearsUntilRetirement(1950, 'celso'));
